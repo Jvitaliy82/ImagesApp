d3e7b9f9023bd31a20bf4208dadfe195e6048921
Jvitaliy <jvit82@gmail.com>
2021-01-08 23:38:05 +0700

    add navGraph

---
+++ app/src/main/java/com/jdeveloperapps/imagesapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.jdeveloperapps.imagesapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.jdeveloperapps.imagesapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
